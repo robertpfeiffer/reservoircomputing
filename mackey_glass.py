6535d0bf83be2c8544c487d9af26e094f1019a7a
Robert Pfeiffer <pfeiffer.robert@gmail.com>
2012-08-27 11:27:52 +0200

    mackey-glass fix

--- mackey_glass.py
+++ mackey_glass.py
@@ -1,13 +1,14 @@
 import numpy
+import random
 
 def mackey_glass(beta, gamma, n, tau, x, dt):
     # ring buffer
     buf_size = int(tau/dt)
-    buf      = list(0 for a in range(buf_size))
+    buf      = [abs(random.gauss(0.5,0.1)) for a in range(buf_size)]
     buf_idx  = 0
     while True:
         x_tau = buf[buf_idx]
-        dx_dt = beta *x_tau/(1+x_tau)**n - gamma*x
+        dx_dt = beta *x_tau/(1+x_tau**n) - gamma*x
         x = x + dx_dt*dt
         buf[buf_idx] = x
         buf_idx = (1 + buf_idx) % buf_size
