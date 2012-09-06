e673af3cf31c75abd0f071b98f2f7d313e64dd30
Robert Pfeiffer <pfeiffer.robert@gmail.com>
2012-09-06 21:10:03 +0200

    more training sets

--- example.py
+++ example.py
@@ -4,7 +4,12 @@ import shelve
 import numpy
 import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
+
+def triangle(n):
+    return n - int(n)
+
+def squarewave(n):
+    return int(n) % 2
 
 def plot_ESN_run(machine,input,target=None,w_out=None,n_plot=1000,n_throwaway=0,jumpstart=0):
         plot_in=list(itertools.islice(input,n_plot+n_throwaway))
@@ -41,6 +46,89 @@ def plot_ESN_run(machine,input,target=None,w_out=None,n_plot=1000,n_throwaway=0,
 
 
         plt.show()
+
+def wave_training_data(sim_length):
+    timescale=100.0
+    u_input=([math.sin(t/timescale)] for t in range(sim_length))
+    u_target=([math.sin(t/timescale + 0.75),math.sin(t/timescale + 1.75),squarewave(t/timescale/math.pi),1] for t in range(sim_length))
+
+    timescale=140.0
+    u_input2=([math.sin(t/timescale)] for t in range(sim_length))
+    u_target2=([math.sin(t/timescale + 0.75),math.sin(t/timescale + 1.75),squarewave(t/timescale/math.pi),1] for t in range(sim_length))
+
+    timescale=160.0
+    u_input3=([math.sin(t/timescale)] for t in range(sim_length))
+    u_target3=([math.sin(t/timescale + 0.75),math.sin(t/timescale + 1.75),squarewave(t/timescale/math.pi),1] for t in range(sim_length))
+
+    timescale=180.0
+    u_input4=([math.sin(t/timescale)] for t in range(sim_length))
+    u_target4=([math.sin(t/timescale + 0.75),math.sin(t/timescale + 1.75),squarewave(t/timescale/math.pi),1] for t in range(sim_length))
+
+    return [(u_input,u_target),(u_input2,u_target2),
+            (u_input3,u_target3),(u_input4,u_target4)]
+
+
+def pulse_data(length,offset,duration):
+    u_input=([1 if offset < i < offset+duration else 0] for i in range(length))
+    return u_input
+
+def pulse_training_data(length,offset,offset2,duration):
+    pairs=[]
+    for j in range(10):
+        delay=numpy.random.poisson(offset)
+        u_input=([1 if delay+offset < i < delay+offset+duration else 0] for i in range(length))
+        u_target=([1 if delay+offset2 < i < delay+offset2+duration else 0] for i in range(length))
+        pairs.append((u_input,u_target))
+    return pairs
+
+def wave_training_data_hard(sim_length):
+    pairs=[]
+    for timescale in [100.0,140.0,160.0,180.0,80.0]:
+    	u_input=([math.sin(t/timescale)] for t in range(sim_length))
+    	u_target=([math.sin(0.75+t/(timescale/n**2))+m for n,m in [(1,3),(1.5,5),(2,7),(3,9)]] for t in range(sim_length))
+        pairs.append((u_input,u_target))
+    return pairs
+
+
+def wave_training_data_timescale(sim_length,timescale):
+    pairs=[]
+    for timescale in [timescale * 1.1, timescale * 0.9, timescale * 0.95, timescale * 1.05]:
+        u_input=([math.sin(t/timescale)] for t in range(sim_length))
+        u_target=([math.sin(0.75+t/timescale)] for t in range(sim_length))
+        pairs.append((u_input,u_target))
+    return pairs
+
+def square_training_data_timescale(sim_length,timescale):
+    pairs=[]
+    for timescale in [timescale * 1.1, timescale * 0.9, timescale * 0.95, timescale * 1.05]:
+        u_input=([squarewave(t/timescale)] for t in range(sim_length))
+        u_target=([squarewave(0.5+t/timescale)] for t in range(sim_length))
+        pairs.append((u_input,u_target))
+    return pairs
+
+def wave_test_data(sim_length):
+    timescale=120.0
+    u_input=([math.sin(t/timescale)] for t in range(sim_length))
+    u_target=([math.sin(t/timescale + 0.75),math.sin(t/timescale + 1.75),squarewave(t/timescale/math.pi),1] for t in range(sim_length))
+
+    return u_input,u_target
+
+def wave_test_data_hard(sim_length):
+    timescale=120.0
+    u_input=([math.sin(t/timescale)] for t in range(sim_length))
+    u_target=([math.sin(0.75+t/(timescale/n**2))+m for n,m in [(1,3),(1.5,5),(2,7),(3,9)]] for t in range(sim_length))
+
+    return u_input,u_target
+
+def wave_test_data_timescale(sim_length,timescale):
+    u_input=([math.sin(t/timescale)] for t in range(sim_length))
+    u_target=([math.sin(0.75+t/timescale)] for t in range(sim_length))
+    return u_input,u_target
+
+def square_test_data_timescale(sim_length,timescale):
+    u_input=([squarewave(t/timescale)] for t in range(sim_length))
+    u_target=([squarewave(0.5+t/timescale)] for t in range(sim_length))
+    return u_input,u_target
 
 if raw_input("run Mackey-Glass?[y/n] ")=="y":
     from mackey_glass  import mackey_glass
