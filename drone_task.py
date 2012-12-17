80d7e5df8eb22472ba4546520cf2b9817bd35c22
Robert Pfeiffer <pfeiffer.robert@gmail.com>
2012-12-17 13:30:23 +0100

    drone task

--- drone_task.py
+++ drone_task.py
@@ -13,28 +13,33 @@ def run_drone_task():
     data[:,0:3]=data[:,0:3]/10.0
     data[:,3:6]=data[:,3:6]/10.0
 
-    diff = 5
+    size,dim=data.shape
+    diff = 1
     split=600
-    size=1000
 
-    train_input=data[0:split-diff,:]
-    train_target=data[diff:split,:]
+    unshifted = data[0:size-diff,:]
+    shifted = data[diff:size,:]
+    changes = shifted - unshifted
+
+    train_input=unshifted[:split,:]
+    train_target=changes[:split,:]
     print train_input.shape
     print train_target.shape
 
-    test_input=data[split-diff:size-diff,:]
-    test_target=data[split:size,:]
+    test_input=unshifted[split:,:]
+    test_target=changes[split:,:]
     print test_input.shape
     print test_target.shape
 
-    test_input=data[0:size-diff,:]
-    test_target=data[diff:size,:]
+    test_input=unshifted
+    test_target=changes
     print test_input.shape
     print test_target.shape
 
     print "train machine..."
-    machine = ESN(10,400,leak_rate=0.1,frac_exc=0.8,gamma=logistic)
+    #machine = ESN(20,400,leak_rate=1.0,frac_exc=0.5,gamma=np.tanh)
     #trainer = FeedbackReadout(machine,LinearRegressionReadout,ridge=1)
+    machine = ESN(20,400,leak_rate=0.8,frac_exc=0.7,gamma=logistic)
     trainer = LinearRegressionReadout(machine,ridge=1)
 
     trainer.train(train_input, train_target)
@@ -45,9 +50,6 @@ def run_drone_task():
     for dim in range(10):
         plt.plot(test_target[:,dim].ravel())
         plt.plot(prediction[:,dim].ravel())
-        plt.plot(test_input[:,dim].ravel()-test_target[:,dim].ravel())
-        plt.plot(test_input[:,dim].ravel()-prediction[:,dim].ravel())
-
         plt.show()
     mse = Oger.utils.mse(prediction,test_target)
     print "MSE:", mse
