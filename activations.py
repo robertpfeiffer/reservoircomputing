ae6b709b2c966a274e897da1aa4c91cb136c424f
Robert Pfeiffer <rincewind@tombstone.(none)>
2013-01-27 14:51:11 +0100

    add intrinsic plasticity
    A learning activation function that models intrinsic plasticity (plasticity of 
    the soma). Learning happens on-line by stochastic gradient descent.
    The activation function learns to produce activations that follow an exponential
    distribution.
    
    Intrinsic plasticity is unsupervised reservoir tuning. The learning can be turned
    off at any time.

--- activations.py
+++ activations.py
@@ -13,10 +13,28 @@ def smoothstep(matrix):
     return p1-p2
 
 def asymmetric_sigmoid(matrix):
-    """logistic function, applied element-wise to a matrix.
-    Can be used as a sigmoidal activation function"""
+    """this function is 0 at 0 but grows more quickly on the positive side"""
     return numpy.tanh(matrix)/2+smoothstep(matrix)
 
 def softmax(vector):
     exp = numpy.exp(vector)
     return exp/numpy.sum(exp)
+
+def fermi(x,a,b):
+    return numpy.reciprocal(1+numpy.exp(-a*x-b))
+
+def ip_fermi(gamma,mean,n):
+    def inner(x):
+        y = fermi(x, inner.a, inner.b)
+        if inner.learn:
+            delta_b = gamma * (1 - (2+1/mean)*y + 1/mean*y*y)
+            delta_a = gamma * 1/inner.a + x*delta_b
+            inner.a += delta_a
+            inner.b += delta_b
+        return y
+
+    inner.a=numpy.ones(n)
+    inner.b=numpy.ones(n)*0.1
+    inner.learn=True
+
+    return inner
