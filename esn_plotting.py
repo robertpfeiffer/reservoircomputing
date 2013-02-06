9e9f08f8610f22b86d98613aef7600f553de43d6
Robert Pfeiffer <rincewind@tombstone.(none)>
2013-02-06 10:58:25 +0100

    plotting

--- esn_plotting.py
+++ esn_plotting.py
@@ -12,10 +12,12 @@ def run_perturbation(esn,data1=None):
     else:
         data1=data1[:500,:]
     data2=numpy.copy(data1)
-    data2[50:100,:]=1
-    data1[50:100,:]=0
+    data2[90:100,:]=1
+    data1[90:100,:]=0
     s1=esn.run_batch_feedback(data1)
     s2=esn.run_batch_feedback(data2)
+    d["input"] = data1
+    d["echo_states"] = s1
     d["difference"] = abs(s2-s1)
     d["sum_difference"] = numpy.sum(abs(s2-s1),axis=1)
 
@@ -27,14 +29,13 @@ def set_weights(weights):
     d["weights"]=weights
 
 def run_task(readout,input,target):
-    d["output"]=readout.predict(input)[1]
+    d["output"]=readout.predict(input)
     d["target"]=target
     d["input"]=input
 
 def show_task():
-    for dim in range(d["target"].shape[1]): 
-        plt.plot(d["target"][:1000,dim].ravel())
-        plt.plot(d["output"][:1000,dim].ravel())
+    plt.plot(d["target"][0,:1000].ravel())
+    plt.plot(d["output"][0,:1000].ravel())
     plt.show()
 
 def plot_activations(max_duration=1000,max_nodes=300):
@@ -81,33 +82,44 @@ def plot_spectrum(max_freq=1000,max_nodes=300):
     plt.pcolormesh(spectrum[:max_freq,:].T)
     plt.show()
 
-def plot_activations_weighted(max_duration=1000,max_nodes=300):
-    echo_states_weighted = numpy.zeros(d["echo_states"].shape)
-    echo_states = d["echo_states"]
+def plot_activations_weighted(max_duration=1000,max_nodes=300,out_unit=0):
+    echo_states = numpy.copy(d["echo_states"])
     x,y = echo_states.shape
     w_out=d["weights"]
+    for i in range(echo_states.shape[0]):
+        echo_states[i,:] = echo_states[i,:]*w_out[1:,out_unit]
     if x > max_duration:
         echo_states = echo_states[0:max_duration,:]
     if y > max_nodes:
         echo_states = echo_states[:,0:max_nodes]
-    for i in range(echo_states.shape[0]):
-        for j in range(w_out.shape[1]):
-            echo_states_weighted[i,:] += echo_states[i,:]*w_out[1:,j]
     echo_states = echo_states.T
-    plt.pcolormesh(echo_states_weighted,cmap="bone")
+    plt.pcolormesh(echo_states,cmap="bone")
     plt.show()
 
-def plot_spectrum_weighted(max_freq=1000,max_nodes=300):
+def plot_spectrum_weighted(max_freq=1000,max_nodes=300,out_unit=0):
     echo_states = d["echo_states"]
     x,y = echo_states.shape
     w_out=d["weights"]
     spectrum=numpy.fft.rfft(echo_states[:max_freq,:], axis = 0)
-    spectrum_weighted = numpy.zeros(spectrum.shape)
     spectrum[0,:]=0 # we do not care about constants
     spectrum=numpy.abs(spectrum)
     for i in range(spectrum.shape[0]):
-        for j in range(w_out.shape[1]):
-            spectrum_weighted[i,:] = spectrum[i,:]*w_out[1:,j]
+        spectrum[i,:] = spectrum[i,:]*w_out[1:,out_unit]
     #spectrum = spectrum*w_out[1:,out_unit]
-    plt.pcolormesh(spectrum_weighted[:max_freq,:].T)
+    plt.pcolormesh(spectrum[:max_freq,:].T)
     plt.show()
+
+def plot_activation_distribution():
+    echo_states = d["echo_states"]
+    x,y = echo_states.shape
+    
+
+def calc_loops(m):
+    x,y = m.shape
+    m1=numpy.eye(x)
+    diag=numpy.diag_indices_from(m)
+    for i in range(x):
+        m1=numpy.dot(m1,m)
+        yield len(numpy.nonzero(m1[diag])[0])
+    
+        
